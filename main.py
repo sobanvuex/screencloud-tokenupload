95b99ed7f067a67749f1443f50fcecb13bb2e795
SobanVuex <soban.vue@gmail.com>
2014-05-15 11:18:57 -0400

    Small refactoring - more descriptive name - simpler settings - response error handling

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PythonQt.QtGui import QWidget, QDialog
 from PythonQt.QtUiTools import QUiLoader
 
 
-class UrlUploader():
+class TokenUploader():
 	def __init__(self):
 		self.uil = QUiLoader()
 		self.loadSettings()
@@ -20,7 +20,6 @@ class UrlUploader():
 		self.loadSettings()
 		self.settingsDialog.group_url.input_token.text = self.url_token
 		self.settingsDialog.group_url.input_address.text = self.url_address
-		self.settingsDialog.group_url.input_attribute.text = self.url_attribute
 		self.settingsDialog.group_screenshot.input_name.text = self.name_format
 		self.settingsDialog.open()
 
@@ -30,10 +29,9 @@ class UrlUploader():
 	def loadSettings(self):
 		settings = QSettings()
 		settings.beginGroup("uploaders")
-		settings.beginGroup("urluploader")
+		settings.beginGroup("tokenupload")
 		self.url_token = settings.value("url-token", "")
 		self.url_address = settings.value("url-address", "")
-		self.url_attribute = settings.value("url-attribute", "href")
 		self.name_format = settings.value("name-format", "Screenshot (%Y-%m-%d %H-%M-%S)")
 		settings.endGroup()
 		settings.endGroup()
@@ -41,10 +39,9 @@ class UrlUploader():
 	def saveSettings(self):
 		settings = QSettings()
 		settings.beginGroup("uploaders")
-		settings.beginGroup("urluploader")
+		settings.beginGroup("tokenupload")
 		settings.setValue("url-token", self.settingsDialog.group_url.input_token.text)
 		settings.setValue("url-address", self.settingsDialog.group_url.input_address.text)
-		settings.setValue("url-attribute", self.settingsDialog.group_url.input_attribute.text)
 		settings.setValue("name-format", self.settingsDialog.group_screenshot.input_name.text)
 		settings.endGroup()
 		settings.endGroup()
@@ -66,19 +63,22 @@ class UrlUploader():
 		q_buff.close()
 		image = q_ba.toBase64().data()
 		data = json.dumps({'token': self.url_token, 'name': name, 'image': image})
-		headers = {"Content-Type": "application/json"}
+		headers = {'Content-Type': 'application/json'}
 		request = urllib2.Request(self.url_address, data, headers)
 		try:
 			reply = urllib2.urlopen(request)
-		except Exception as e:
+		except Exception:
 			ScreenCloud.setError("Could not upload to: " + self.url_address)
 			return False
 		try:
 			response = reply.read()
 			data = json.loads(response)
-			url = data.get(self.url_attribute)
+			error = data.get('error')
+			url = data.get('href')
+			if error:
+				raise Exception(error)
 			ScreenCloud.setUrl(url)
 		except Exception as e:
-			ScreenCloud.setError("Could not upload to: " + self.url_address + "\n" + e.message)
+			ScreenCloud.setError("Could not upload to: " + self.url_address + "\nError: " + e.message)
 			return False
 		return True
